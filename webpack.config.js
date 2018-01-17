18f0a0f8fe189900f076c06b61f6eaa282a7073d
ben awad <benawad97@gmail.com>
2018-01-17 15:22:00 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+};
